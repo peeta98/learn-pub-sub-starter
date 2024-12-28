5bced8db3901596149dda676179c5a465c529e65
Pedro Moreira <pedro.moreira@emma-sleep.com>
2024-12-28 06:18:20 -0100

    Finish until chapter 3, Lesson 6 - Decoupling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,80 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("could not connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Peril game server connected to RabbitMQ!")
+
+	gamelogic.PrintServerHelp()
+
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
+
+		switch words[0] {
+		case "pause":
+			publishCh, err := conn.Channel()
+			if err != nil {
+				log.Fatalf("could not create channel: %v", err)
+			}
+
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: true,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+
+			fmt.Println("Pause message sent!")
+
+		case "resume":
+			publishCh, err := conn.Channel()
+			if err != nil {
+				log.Fatalf("could not create channel: %v", err)
+			}
+
+			err = pubsub.PublishJSON(
+				publishCh,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{
+					IsPaused: false,
+				},
+			)
+			if err != nil {
+				log.Printf("could not publish time: %v", err)
+			}
+
+			fmt.Println("Resume message sent!")
+
+		case "quit":
+			fmt.Println("Exiting server, bye!")
+			return
+
+		default:
+			fmt.Print("Unknown command, try again.")
+		}
+	}
 }
